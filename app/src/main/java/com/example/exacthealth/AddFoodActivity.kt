7a8a709de129360e5bb58c68ca74909a0f799e4d
SamanKhamesian <saman.khamesian@gmail.com>
2024-04-03 23:49:56 -0700

    Enable pick images from gallery

--- app/src/main/java/com/example/exacthealth/AddFoodActivity.kt
+++ app/src/main/java/com/example/exacthealth/AddFoodActivity.kt
@@ -3,10 +3,16 @@ package com.example.exacthealth
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
 import android.widget.RelativeLayout
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import java.text.SimpleDateFormat
@@ -15,6 +21,8 @@ import java.util.Locale
 
 class AddFoodActivity : AppCompatActivity()
 {
+    private lateinit var pickImagesLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -25,6 +33,9 @@ class AddFoodActivity : AppCompatActivity()
 
         val foodTimeLayout = findViewById<TextInputLayout>(R.id.food_time_layout)
         val foodTimeInput = findViewById<TextInputEditText>(R.id.food_time_input)
+
+        val pickImagesButton = findViewById<Button>(R.id.pick_images_button)
+        val selectedImagesButton = findViewById<Button>(R.id.selected_images_button)
 
         val calendarButton = findViewById<RelativeLayout>(R.id.back_to_calendar_layout)
         val favoriteFoodButton = findViewById<ConstraintLayout>(R.id.add_from_favorites_layout)
@@ -39,6 +50,37 @@ class AddFoodActivity : AppCompatActivity()
             setTime(foodTimeInput)
         }
 
+        pickImagesButton.setOnClickListener {
+            openGalleryForMultipleImages()
+        }
+
+        // Set up the ActivityResultLauncher
+        pickImagesLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK)
+            {
+                val selectedImagesUriList = mutableListOf<Uri>()
+                val intent = result.data
+                intent?.clipData?.let { clipData ->
+                    for (i in 0 until clipData.itemCount)
+                    {
+                        val uri = clipData.getItemAt(i).uri
+                        selectedImagesUriList.add(uri)
+                    }
+                } ?: run {
+                    val uri = intent?.data
+                    uri?.let {
+                        selectedImagesUriList.add(uri)
+                    }
+                }
+
+                // Do something with the list of selected image URIs
+                println("Selected ${selectedImagesUriList.size} images")
+                // Enable the button
+                selectedImagesButton.isEnabled = true
+                selectedImagesButton.setTextColor(ContextCompat.getColor(this, R.color.red))
+            }
+        }
+
         calendarButton.setOnClickListener {
             val intent = Intent(this, CalendarActivity::class.java)
             startActivity(intent)
@@ -50,6 +92,13 @@ class AddFoodActivity : AppCompatActivity()
         }
     }
 
+    private fun openGalleryForMultipleImages()
+    {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        galleryIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        pickImagesLauncher.launch(galleryIntent)
+    }
+
     private fun setDefaultDateTime(foodDateInput: TextInputEditText, foodTimeInput: TextInputEditText)
     {
         val currentTime = Calendar.getInstance()
@@ -59,6 +108,7 @@ class AddFoodActivity : AppCompatActivity()
         foodTimeInput.setText(timeFormat.format(currentTime.time))
         foodDateInput.setText(dateFormat.format(currentTime.time))
     }
+
     private fun setDate(input: TextInputEditText)
     {
         val calendar = Calendar.getInstance()
@@ -70,7 +120,8 @@ class AddFoodActivity : AppCompatActivity()
             calendar.set(year, monthOfYear, dayOfMonth)
             val dateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault())
             val selectedDate = dateFormat.format(calendar.time)
-            input.setText(selectedDate)}, y, m, d)
+            input.setText(selectedDate)
+        }, y, m, d)
         datePickerDialog.show()
     }
 
@@ -82,7 +133,8 @@ class AddFoodActivity : AppCompatActivity()
 
         val timePickerDialog = TimePickerDialog(this, R.style.DialogTheme, { _, selectedHourOfDay, selectedMinute ->
             val timeFormat = String.format("%02d:%02d", selectedHourOfDay, selectedMinute)
-            input.setText(timeFormat)}, hourOfDay, minute, true)
+            input.setText(timeFormat)
+        }, hourOfDay, minute, true)
         timePickerDialog.show()
     }
 }
